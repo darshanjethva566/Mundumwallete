bf1032bbdcdc3e3e7f37f51ceef6874ce9cd3d61
Adityachavda <adityachavda2000@gmail.com>
2023-12-23 20:46:14 +0530

    improved project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:mundumwallet/Splash%20Screen/splash_screen.dart';
+import 'package:mundumwallet/routes/pages.dart';
 
 void main() {
 
@@ -13,14 +14,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Mundum Wallet',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+        minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return child!;
+      },
+      child: GetMaterialApp(
+        title: 'Mundum Wallet',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        getPages: AppPages.pages,
+        initialRoute: Routes.initial,
       ),
-      home: splash_screen(),
     );
   }
 }
